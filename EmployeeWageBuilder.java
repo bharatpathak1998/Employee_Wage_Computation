b25a0d4994395f3fae8d9c3afee3cf2965b064e8
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-10 13:52:47 +0530

    Added : Employee Monthly Wage Condition Using Method Call

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,20 +1,19 @@
-package UC6;
+package UC7;
 
 import java.util.Random;
 
 public class EmployeeWageBuilder {
-    public static final int FullTime=1;
-    public static final int PartTime=2;
-    public static final int wagePerHour=20;
-    public static final int NumOfWorkingDays=20;
-    public static final int MaxWorkingHrs=100;
+    public static final int FullTime = 1;
+    public static final int PartTime = 2;
+    public static final int wagePerHour = 20;
+    public static final int NumOfWorkingDays = 20;
+    public static final int MaxWorkingHrs = 100;
 
-    public static void main(String[] args) {
-
-        int workingHrs, empWage, totalWorkingHrs=0, totalWorkingDays=0;
-        while(totalWorkingHrs<=MaxWorkingHrs && totalWorkingDays<NumOfWorkingDays) {
+    public void computeEmployeeWage() {
+        int workingHrs, empWage, totalWorkingHrs = 0, totalWorkingDays = 0;
+        while (totalWorkingHrs <= MaxWorkingHrs && totalWorkingDays < NumOfWorkingDays) {
             totalWorkingDays++;
-            System.out.println("\nDay: "+totalWorkingDays);
+            System.out.println("\nDay: " + totalWorkingDays);
 
             Random random = new Random();
             int empCheck = random.nextInt(3);
@@ -36,9 +35,13 @@ public class EmployeeWageBuilder {
             System.out.println("Employee working hours = " + workingHrs);
 
             empWage = workingHrs * wagePerHour;
-            System.out.println("Employee Wage = "+empWage);
+            System.out.println("Employee Wage = " + empWage);
         }
         int totalEmpWage = totalWorkingHrs * wagePerHour;
-        System.out.println("\nTotal Employee Wage = "+totalEmpWage);
+        System.out.println("\nTotal Employee Wage = " + totalEmpWage);
+    }
+    public static void main(String[] args) {
+        EmployeeWageBuilder obj = new EmployeeWageBuilder();
+        obj.computeEmployeeWage();
     }
 }
