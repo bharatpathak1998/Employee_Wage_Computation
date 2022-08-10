31e0e1b826df8e2057706aab6afe19ce8c3568ce
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-10 13:18:13 +0530

    Added : To Check Daily Employee Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,16 +1,20 @@
-package UC1;
+package UC2;
 
 import java.util.Random;
 
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         int IS_PRESENT=1;
+        int wagePerHour=20;
+        int workingHrs=8;
 
         Random random = new Random();
         int empCheck = random.nextInt(2);
 
         if(empCheck==IS_PRESENT) {
+            int empWage = workingHrs * wagePerHour;
             System.out.println("Employee is present");
+            System.out.println("Employee Wage = "+empWage);
         } else {
             System.out.println("Employee is absent");
         }
